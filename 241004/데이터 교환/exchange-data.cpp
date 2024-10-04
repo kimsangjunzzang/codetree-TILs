f47ed824b2cc9c64a70c8a6e91c86007727a8b9b
kimsangjunzzang <84498457+kimsangjunzzang@users.noreply.github.com>
2024-10-04 12:20:09 +0900

    Title: 데이터 교환, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241004/데이터 교환/exchange-data.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a = 5;
+    int b = 6;
+    int c = 7;
+
+    b =a; c = b; a = c;
+    cout <<a<<endl;
+    cout <<b<<endl;
+    cout <<c <<endl;
+    return 0;
+}
