6f73ebee79323eddc0dfabe2a42c9d41a6755406
kimsangjunzzang <84498457+kimsangjunzzang@users.noreply.github.com>
2024-10-04 12:16:30 +0900

    Title: 두 실수의 곱, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241004/두 실수의 곱/the-product-of-two-real-numbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    double d1 = 5.26;
+    double d2 = 8.27;
+
+    cout<<fixed;
+    cout.precision(3);
+ 
+
+    cout << d1 * d2 <<endl;
+    return 0;
+}
