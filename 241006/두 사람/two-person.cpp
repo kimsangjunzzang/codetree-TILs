725428311895196056bb79a6606e0fa5eb1e3c3f
kimsangjunzzang <84498457+kimsangjunzzang@users.noreply.github.com>
2024-10-06 16:40:17 +0900

    Title: 두 사람, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241006/두 사람/two-person.cpp
+++ 241006/두 사람/two-person.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 int main() {
     // 여기에 코드를 작성해주세요.
-
+int cnt = 0;
     for ( int i=0;i<2;i++) {
     int num; cin >> num;
     char gender; cin >>gender;
-    int cnt = 0;
+    
 
     if(num >19 && gender == 'M') {
         cout << 1;
@@ -16,6 +16,7 @@ int main() {
         cnt ++;
         }
     }
+
     if(cnt == 2) cout << 0;
     return 0;
 }
