51fb0c0c288693bf721e858a9e58af3906f7b874
kimsangjunzzang <84498457+kimsangjunzzang@users.noreply.github.com>
2024-10-06 16:25:51 +0900

    Title: 살 수 있는 물건, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241006/살 수 있는 물건/things-able-to-buy.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int num; cin >>num;
+    if(num >= 3000) cout << "book";
+    else cout <<"mask";
+    return 0;
+}
