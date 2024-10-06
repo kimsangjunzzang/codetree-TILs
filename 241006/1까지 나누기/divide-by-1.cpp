a7df6eda7b3d806ac55181b67080f9364d622843
kimsangjunzzang <84498457+kimsangjunzzang@users.noreply.github.com>
2024-10-06 17:20:39 +0900

    Title: 1까지 나누기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241006/1까지 나누기/divide-by-1.cpp
+++ 241006/1까지 나누기/divide-by-1.cpp
@@ -4,12 +4,15 @@ using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
     int n; cin >>n;
-    int cnt =1;
+    int cnt =0;
     int i =1;
-    while(n / i >= 1) {
+
+    while(1) {
         n /= i;
+        // cout << n;
         i++;
         cnt++;
+        if(n < 1) break;
     }
     cout << cnt;
     return 0;
