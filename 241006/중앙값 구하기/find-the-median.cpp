719af118fd029781899d756d19313d52e2e48015
kimsangjunzzang <84498457+kimsangjunzzang@users.noreply.github.com>
2024-10-06 16:42:18 +0900

    Title: 중앙값 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241006/중앙값 구하기/find-the-median.cpp
+++ 241006/중앙값 구하기/find-the-median.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
 using namespace std;
 
 int main() {
     // 여기에 코드를 작성해주세요.
     vector<int>v(3);
+
     for ( int i=0;i<3;i++) {
         cin >> v[i];
     }
+
     sort(v.begin(),v.end());
     cout << v[1] <<endl;
 
