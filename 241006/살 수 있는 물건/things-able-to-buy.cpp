e3bf4f0286883d54d959125fa7ed9fe652ca29d9
kimsangjunzzang <84498457+kimsangjunzzang@users.noreply.github.com>
2024-10-06 16:26:22 +0900

    Title: 살 수 있는 물건, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 241006/살 수 있는 물건/things-able-to-buy.cpp
+++ 241006/살 수 있는 물건/things-able-to-buy.cpp
@@ -5,6 +5,7 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int num; cin >>num;
     if(num >= 3000) cout << "book";
-    else cout <<"mask";
+    else if(num >= 1000) cout <<"mask";
+    else "no"
     return 0;
 }
